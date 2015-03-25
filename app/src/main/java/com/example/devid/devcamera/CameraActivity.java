e0ada8fb01ff27ea1fe76f551c067bb6259561aa
Devid Farinelli <devid.farinelli@gmail.com>
2015-03-26 00:35:07 +0100

    Working take picture

--- app/src/main/java/com/example/devid/devcamera/CameraActivity.java
+++ app/src/main/java/com/example/devid/devcamera/CameraActivity.java
@@ -1,17 +1,95 @@
 package com.example.devid.devcamera;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by devid on 25/03/15.
  */
 public class CameraActivity extends Activity {
-
+    private static final String TAG = "MyActivity";
     private Camera mCamera;
     private CameraPreview mPreview;
+    private PictureCallback mPicture = new PictureCallback() {
+
+        public static final int MEDIA_TYPE_IMAGE = 1;
+
+        /** Create a file Uri for saving an image or video */
+        private Uri getOutputMediaFileUri(int type){
+            return Uri.fromFile(getOutputMediaFile(type));
+        }
+
+        /** Create a File for saving an image or video */
+        private File getOutputMediaFile(int type){
+            // To be safe, you should check that the SDCard is mounted
+            // using Environment.getExternalStorageState() before doing this.
+
+            File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES), "MyCameraApp");
+            // This location works best if you want the created images to be shared
+            // between applications and persist after your app has been uninstalled.
+
+            // Create the storage directory if it does not exist
+            if (! mediaStorageDir.exists()){
+                if (! mediaStorageDir.mkdirs()){
+                    Log.d("MyCameraApp", "failed to create directory");
+                    return null;
+                }
+            }
+
+            // Create a media file name
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            File mediaFile;
+            if (type == MEDIA_TYPE_IMAGE){
+                mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                        "IMG_"+ timeStamp + ".jpg");
+            } else {
+                return null;
+            }
+
+            return mediaFile;
+        }
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions: ");
+                return;
+            } else {
+                Log.d(TAG, "Errddssddsor creating media file, check storage permissions: ");
+
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+            Log.d(TAG, "FileFtoo");
+            sendPic(pictureFile.getAbsolutePath());
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,10 +103,40 @@ public class CameraActivity extends Activity {
         mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+
+        // Add a listener to the Capture button
+        Button captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        mCamera.takePicture(null, null, mPicture);
+                    }
+                }
+        );
+
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();              // release the camera immediately on pause event
+    }
+
     @Override
     public void onDestroy() {
-        mCamera.release();
         super.onDestroy();
+        mCamera.release();
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
+    }
+    public void sendPic(String path){
+
     }
 }
